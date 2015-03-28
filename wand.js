2ce864a80ac4eda49a5d3821a862b4f70c10937e
Larry Blyth <lblyth2@uwo.ca>
2015-03-28 18:08:25 -0400

    trying to get audio working

--- wand.js
+++ wand.js
@@ -1,4 +1,10 @@
 var lastGesture="none";
+var src = chrome.extension.getUrl("theme_song.mp3");
+var element = "<audio controls><source src="+src+" type='audio/mpeg'>Your browser does not support the audio element.</audio>";
+//var element = '<embed type='application/x-shockwave-flash'flashvars='audio/theme_song' src='http://www.google.com/reader/ui/3523697345-audio-player.swf' width='400' height='27' quality='best'></embed>";
+$('body').append(element);
+
+HarryPottify(); // for testing
 
 // Leap.loop uses browser's requestAnimationFrame
 var options = { enableGestures: true };
@@ -55,6 +61,7 @@ function CheckForGesture(frame) {
 
 function ClockwiseCircle() {
     console.log('ClockwiseCircle detected');
+    HarryPottify();
 }
 function CounterClockwiseCircle() {
     console.log('CounterClockwiseCircle detected');
@@ -62,7 +69,7 @@ function CounterClockwiseCircle() {
 function KeyTap() {
     console.log('KeyTap detected');
 }
-function CircleScreenTap() {
+function CircleScreenTap() { //will do circle action too :(
     console.log('CircleScreenTap detected');
 }
 function ScreenTap() {
@@ -72,7 +79,7 @@ function ScreenTap() {
 function Swipe() {
     console.log('Swipe detected');
 }
-function CircleSwipe() {
+function CircleSwipe() { //will do circle action too :(
     console.log('CircleSwipe detected');
 }
 
@@ -89,4 +96,24 @@ function babbliomus() {
 		font-family: Wingdings;\
 	}\
 	</style>")
+}
+
+function HarryPottify() {
+    //replace all images with harry potter images
+    console.log('HarryPottify!');
+    var imageSource = [
+        "https://38.media.tumblr.com/36318e459e66739f1a481e03585f40aa/tumblr_nlw0laEc591si2ypro1_500.gif",
+        "https://38.media.tumblr.com/f9940450a1e0d5db482f020361a21803/tumblr_nlw0laEc591si2ypro2_500.gif",
+        "https://40.media.tumblr.com/7d3f2d2e34fea6a7d591719c9adf19d5/tumblr_nlxtp5ZdbV1qzcmp3o1_500.jpg",
+        "https://33.media.tumblr.com/8ee2c2879a78309915ad32ae858082b1/tumblr_nlw3dhgmTy1u9p9x4o2_500.gif",
+        "https://38.media.tumblr.com/3b22871c3236db0f0492b9aa582abaf5/tumblr_nlw4cjWrQx1r7mqm8o4_250.gif",
+        "https://33.media.tumblr.com/833999cd2dd6ed8e92e89608ed73c771/tumblr_nlw9fvEZVV1rmbnsmo1_250.gif"
+    ];
+    var i = 0;
+    $("img").each(function() {
+        if(i==imageSource.length) i = 0;
+        $(this).attr("src", imageSource[i]);
+        i++;
+    });
+    //$("img").attr("src", "http://www.anglotopia.net/wp-content/uploads/2015/02/171c8a0a-4b73-4873-bdcd-18d7c4c50e5e.jpg");
 }
